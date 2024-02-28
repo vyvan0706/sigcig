27b539f208f293c1dced86f1078257c8f8a5087f
vyvan0706 <150523177+vyvan0706@users.noreply.github.com>
2024-02-28 07:59:15 +0000

    cahnges num 5555

--- Home.py
+++ Home.py
@@ -1,14 +1,38 @@
 import streamlit as st
 import base64
 import time
+import numpy
 from PIL import Image
 from streamlit_extras.buy_me_a_coffee import button
 from streamlit_extras.switch_page_button import switch_page
+from streamlit_extras.markdownlit import mdlit
 st.set_page_config(
     page_title="Home",
     page_icon="👋",
-    layout="wide"
+    layout= 'wide'
 )
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+image_path = 'https://www.airchaser.com/wp-content/uploads/2019/05/BANNER-1800x500-Blog.jpg'
+
+# Define HTML with CSS styling to set the width
+html_str2 =  f"""
+<div style='width: 1080px; height: 200px;'>
+    <img src='{image_path}' alt='Blog Banner' style='width:100%; height:100%; object-fit: cover;'>
+</div>
+"""
+st.markdown(html_str2, unsafe_allow_html=True)
+# Display HTML using st.markdown()
+html_str = "<h1><span style='color: #CDFADB;font-style: italic;'>BLOG</span> <span style='color: #FEFAE0;'>BY</span> <span style='color: #0047AB;'> LEV</span></h1>"
+st.markdown(html_str, unsafe_allow_html=True)
+col1, col2 = st.columns([2, 2])
+
 st.markdown(
         f"""
             <style>
@@ -23,18 +47,21 @@ st.markdown(
             """,
         unsafe_allow_html=True,
     )
-st.title(':grey[_Blog_] by :blue[lev] :sunglasses:')
+
+# Define the markdown content
+col1.header('About me', divider='rainbow')
+
+
 #st.markdown("<h1 style='color: black;'><span style='color: black;font-style: italic;'>Blog</span> <span style='color: black;'>by</span> <span style='color: blue;'>lev</span> 😎</h1>", unsafe_allow_html=True)
 
-_LOREM_IPSUM ='''Hello, I'm Lev. I'm on a journey, traveling from place to place, seeking something elusive yet essential. This blog will document my experiences and reflections along the way. Join me as I navigate through the twists and turns of life's unpredictable road.'''
-def stream_data():
-    for word in _LOREM_IPSUM.split():
-        yield word + " "
-        time.sleep(0.05)
-st.write_stream(stream_data)
-st.image('anh2.png',caption='This is me!',width=300 )
-def example2():
-    want_to_contribute = st.button("Blog entries")
-    if want_to_contribute:
-        switch_page("Blog entries")
-example2()
+#_LOREM_IPSUM ='''Hello, I'm Lev. I'm on a journey, traveling from place to place, seeking something elusive yet essential. This blog will document my experiences and reflections along the way. Join me as I navigate through the twists and turns of life's unpredictable road.'''
+#def stream_data():
+#    for word in _LOREM_IPSUM.split():
+#        yield word + " "
+#        time.sleep(0.05)
+#st.write_stream(stream_data)
+_LOREM_IPSUM = '''<span style="color:black;">Hello, I'm Lev. I'm on a journey, traveling from place to place, seeking something elusive yet essential. This blog will document my experiences and reflections along the way. Join me as I navigate through the twists and turns of life's unpredictable road.</span>'''
+col1.markdown(_LOREM_IPSUM, unsafe_allow_html=True)
+col2.image('anh2.png',caption='This is me!',use_column_width=True )
+col1.page_link("pages/0_Blog_entries.py", label="→ Blog entries here")
+# changd
--- pages/0_Blog_entries.py
+++ pages/0_Blog_entries.py
@@ -3,7 +3,8 @@ import base64
 from PIL import Image
 st.set_page_config(
     page_title="Blog entries",
-    page_icon="👋",
+    page_icon="🔥",
+    layout='wide'
 )
 st.markdown(
         f"""
@@ -20,4 +21,51 @@ st.markdown(
         unsafe_allow_html=True,
     )
 
-tab1,tab2,tab3=st.tabs(['Blog1','Blog2','Blog3','Blog4','Blog5','Blog6','Blog7'])
+t1,t2,t3,t4,t5,t6,t7=st.tabs(['Blog1','Blog2','Blog3','Blog4','Blog5','Blog6','Blog7'])
+with t1:
+
+    text = """
+<span style='color:black;'>
+Today I went to the sulphur spring at Jor with my love - Marina. The sky was blue, just like a turquoise love letter, where the sunlights are countless words I want you to know. In the sky, I saw a flock of storks flew gracefully overhead, and some were finding food for the little storks waiting for their parents. I hope nature can cure what I have lost, but now, the most wonderful scenery can’t even pull myself together… 
+
+I watched you immersed your body obediently in the scummy water. You were laining there, following your eyesight towards a female stork returning to its high nest. If only we were storks
+
+Your voice waked me up from my daydream. Why d’you say that? I asked. Because you never see a stork dying. It’s as though they didn’t die
+
+My dear, i hope i didn’t misunderstand you. I wish I could know how much time you have left with me. I wish I could do something to help you.
+</span>
+"""
+    st.markdown(text, unsafe_allow_html=True)
+with t2:
+    text2 = """
+<span style='color:black;'>
+Today is probably the worst day of my life, Marina.
+I don't understand why life is so unfair to you. 
+you are a beautiful woman, a filial daughter, a good wife and a gentle mother, 
+But why did God give you that terrible disease? leukemia. That makes me keep thinking since I received that shocking news, 
+I don't know how much time I can still be with you, but no matter what, I still love you with all my heart. 
+My love, I will try to be positive, work hard to help pay for medicine and take care of our family more. 
+I only hope that you will be healthy and happy in the future.
+
+</span>
+"""
+    st.markdown(text2, unsafe_allow_html=True)
+with t3:
+    text3 = """
+<span style='color:black;'>
+Marina darling, I'm returning home after days of being by your side without leaving. I thought it was just a nightmare, 
+but when I woke up, you were gone from this world.But everything about you still exists here. The sweet scent of purple 
+daisies still wafts from your shirt... you fell asleep so peacefully just in a blink and couldn't wake up again. The more 
+effortlessly it is for you to go, the greater the pain of losing you.
+
+</span>
+"""
+    st.markdown(text3, unsafe_allow_html=True)
+with t4:
+    text4 = """
+<span style='color:black;'>
+The Baryn sawmill officially closed today. I don't know how long can i live like this. My daughter and I have to depend entirely on my mother. My daughter, she's our precious gem, and she deserves to be happy, not suffer like this. My daughter deserves nothing but the best, and I will stop at nothing to give her the life she deserves.
+
+</span>
+"""
+    st.markdown(text4, unsafe_allow_html=True)
